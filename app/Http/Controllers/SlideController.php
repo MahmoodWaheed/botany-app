d6bb1d990022332f6bf544b992d3e27cb71f0b7d
Mahmoud Waheed <116230767+MahmoodWaheed@users.noreply.github.com>
2023-05-06 15:36:33 +0300

    update SlideController
    update the api for get group by id to return distinct values

--- app/Http/Controllers/SlideController.php
+++ app/Http/Controllers/SlideController.php
@@ -18,17 +18,73 @@ class SlideController extends Controller
 
         if ($group_name) {
             if ($group_name=='Special groups'){
-                return response()->json([
-                    'payload' => DB::table('slides')
-                        ->where('slides.group_name', 'Special groups')
-                        ->join('special_groups', 'slides.id', '=', 'special_groups.id')
-                        ->join('slide_box_numbers', 'slide_box_numbers.slide_id', '=', 'slides.id')
-                        ->join('box_number', 'slide_box_numbers.box_number_id', '=', 'box_number.id')
-                        ->join('slide_slide_ceils', 'slide_slide_ceils.slide_id', '=', 'slides.id')
-                        ->join('slide_ceils', 'slide_slide_ceils.slide_ceils_id', '=', 'slide_ceils.id')
-                        ->select('slides.*', 'special_groups.specimen','box_number.box-number','slide_ceils.ceil_name')
-                        ->get()
-                ]);
+                if ($group_name == 'Special groups') {
+                    return response()->json([
+                        'payload' => DB::table('slides')
+                            ->where('slides.group_name', 'Special groups')
+                            ->join('special_groups', 'slides.id', '=', 'special_groups.id')
+                            ->join('slide_box_numbers', 'slide_box_numbers.slide_id', '=', 'slides.id')
+                            ->join('box_number', 'slide_box_numbers.box_number_id', '=', 'box_number.id')
+                            ->join('slide_slide_ceils', 'slide_slide_ceils.slide_id', '=', 'slides.id')
+                            ->join('slide_ceils', 'slide_slide_ceils.slide_ceils_id', '=', 'slide_ceils.id')
+                            ->select(
+                                'slides.id',
+                                'slides.arabicName',
+                                'slides.count',
+                                'slides.slide_number',
+                                'slides.cupbord',
+                                'slides.english_name',
+                                'slides.image',
+                                'slides.group_name',
+                                'special_groups.specimen',
+                                DB::raw('GROUP_CONCAT(DISTINCT box_number.`box-number` ORDER BY box_number.`box-number` SEPARATOR ",") AS box_numbers'),
+                                DB::raw('GROUP_CONCAT(DISTINCT slide_ceils.`ceil_name` ORDER BY slide_ceils.`ceil_name` SEPARATOR ",") AS ceil_names')
+                            )
+                            ->groupBy('slides.id', 'slides.arabicName', 'slides.count', 'slides.slide_number', 'slides.cupbord', 'slides.english_name', 'slides.image', 'slides.group_name', 'special_groups.specimen')
+                            ->distinct()
+                            ->get()
+                    ]);
+                }
+                
+                // return response()->json([
+                //     'payload' => DB::table('slides')
+                //         ->where('slides.group_name', 'Special groups')
+                //         ->join('special_groups', 'slides.id', '=', 'special_groups.id')
+                //         ->join('slide_box_numbers', 'slide_box_numbers.slide_id', '=', 'slides.id')
+                //         ->join('box_number', 'slide_box_numbers.box_number_id', '=', 'box_number.id')
+                //         ->join('slide_slide_ceils', 'slide_slide_ceils.slide_id', '=', 'slides.id')
+                //         ->join('slide_ceils', 'slide_slide_ceils.slide_ceils_id', '=', 'slide_ceils.id')
+                //         ->select('slides.id', 'slides.arabicName', 'slides.count', 'slides.slide_number', 'slides.cupbord', 'slides.english_name', 'slides.image', 'slides.group_name', 'special_groups.specimen', /*'box_number.box-number',*/ /*'slide_ceils.ceil_name',*/
+                //             DB::raw('GROUP_CONCAT(DISTINCT box_number.`box-number` ORDER BY box_number.`box-number` SEPARATOR ",") AS box_numbers'),
+                //             DB::raw('GROUP_CONCAT(DISTINCT slide_ceils.`ceil_name` ORDER BY slide_ceils.`ceil_name` SEPARATOR ",") AS ceil_names')
+                //         )
+                //         ->groupBy('slides.id', 'slides.arabicName', 'slides.count', 'slides.slide_number', 'slides.cupbord', 'slides.english_name', 'slides.image', 'slides.group_name', 'special_groups.specimen', 'box_number.box-number', 'slide_ceils.ceil_name')
+                //         ->distinct()
+                //         ->get()
+                // ]);
+                // return response()->json([
+                //     'payload' => DB::table('slides')
+                //         ->where('slides.group_name', 'Special groups')
+                //         ->join('special_groups', 'slides.id', '=', 'special_groups.id')
+                //         ->join('slide_box_numbers', 'slide_box_numbers.slide_id', '=', 'slides.id')
+                //         ->join('box_number', 'slide_box_numbers.box_number_id', '=', 'box_number.id')
+                //         ->join('slide_slide_ceils', 'slide_slide_ceils.slide_id', '=', 'slides.id')
+                //         ->join('slide_ceils', 'slide_slide_ceils.slide_ceils_id', '=', 'slide_ceils.id')
+                //         ->select('*'/*, 'special_groups.specimen','box_number.box-number','slide_ceils.ceil_name'*/)
+                //         ->get()
+                // ]);
+                // return response()->json([
+                //     'payload' => DB::table('slides')
+                //         ->where('slides.group_name', 'Special groups')
+                //         ->join('special_groups', 'slides.id', '=', 'special_groups.id')
+                //         ->join('slide_box_numbers', 'slide_box_numbers.slide_id', '=', 'slides.id')
+                //         ->join('box_number', 'slide_box_numbers.box_number_id', '=', 'box_number.id')
+                //         ->join('slide_slide_ceils', 'slide_slide_ceils.slide_id', '=', 'slides.id')
+                //         ->join('slide_ceils', 'slide_slide_ceils.slide_ceils_id', '=', 'slide_ceils.id')
+                //         ->select('*')
+                //         ->distinct()
+                //         ->get()
+                // ]);
             }
 
             return response()->json([
@@ -40,7 +96,23 @@ class SlideController extends Controller
                     ->join('box_number', 'slide_box_numbers.box_number_id', '=', 'box_number.id')
                     ->join('slide_slide_ceils', 'slide_slide_ceils.slide_id', '=', 'slides.id')
                     ->join('slide_ceils', 'slide_slide_ceils.slide_ceils_id', '=', 'slide_ceils.id')
-                    ->select('slides.*', 'section_types.SectionType','box_number.box-number','slide_ceils.ceil_name','others.family','others.latine_name')
+                    ->select(
+                        'slides.id',
+                        'slides.arabicName',
+                        'slides.count',
+                        'slides.slide_number',
+                        'slides.cupbord',
+                        'slides.english_name',
+                        'slides.image',
+                        'slides.group_name',
+                        'section_types.SectionType',
+                        'others.family',
+                        'others.latine_name',
+                        DB::raw('GROUP_CONCAT(DISTINCT box_number.`box-number` ORDER BY box_number.`box-number` SEPARATOR ",") AS box_numbers'),
+                        DB::raw('GROUP_CONCAT(DISTINCT slide_ceils.`ceil_name` ORDER BY slide_ceils.`ceil_name` SEPARATOR ",") AS ceil_names')
+                    )
+                    ->groupBy('slides.id', 'slides.arabicName', 'slides.count', 'slides.slide_number', 'slides.cupbord', 'slides.english_name', 'slides.image', 'slides.group_name', 'section_types.SectionType', 'others.family', 'others.latine_name')
+                    ->distinct()
                     ->get()
             ]);
         }
